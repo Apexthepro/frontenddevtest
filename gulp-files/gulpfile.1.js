637ea15480a6a3efbf778f0b982fa9bcd4d92df0
Apexthepro <avishkadakia1996@gmail.com>
2019-02-20 14:28:24 +0530

    Live reload working

--- gulp-files/gulpfile.1.js
+++ gulp-files/gulpfile.1.js
@@ -1,13 +1,8 @@
 //---------------------------------- Variable Definition Start----------------------------------//
 //Required Variables Start//
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    cleanCSS = require('gulp-clean-css'),
-    sourcemaps = require('gulp-sourcemaps'),
-    concat = require('gulp-concat'),
-    lineec = require('gulp-line-ending-corrector'),
-    autoprefixer = require('gulp-autoprefixer'),
-    uglifycss = require('gulp-uglifycss');
+    sass = require('gulp-sass');
+
 //Environment Configurations Start//
 var projectName = 'frontenddevtest/',
     dev = '../dev/',
@@ -32,13 +27,9 @@ gulp.task('css', function () {
         .pipe(lineec())//line endidng corrector !!!needs to be verified
         .pipe(gulp.dest(dist + '/css/'))//Saving chnages to destination folder
     //  .pipe(browserSync.stream());
-});
+})
 //Gulp CSS End//
 //Gulp Browser Sync Start//
-
-
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
 // Save a reference to the `reload` method
@@ -48,17 +39,18 @@ gulp.task('serve', function () {
 
     // Serve files from the root of this project
     browserSync.init({
-        open: 'external',
-        proxy: 'http://localhost:80/' + projectName + "dev/Home.html",
-        port: 80,
+        server: {
+            baseDir: "./"
+        }
     });
-
     gulp.watch(html).on("change", reload);
+    gulp.watch(scss).on("change", reload);
+    gulp.watch(js).on("change", reload);
 });
 
 //Gulp Browser Sync End//
 //---------------------------------- Task Definition End----------------------------------//
 //---------------------------------- Auto Watch Task Definition Start----------------------------------//
-gulp.task('default', gulp.parallel('serve'));
 
+gulp.task('default', ['serve'], ['css']);
 //---------------------------------- Auto Watch Task Definition End----------------------------------//
